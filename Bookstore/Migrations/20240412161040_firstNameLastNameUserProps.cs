aa5ba7ef13a95a7b6582249dcbeeef04995132c5
DustinGregerson <dustin.gregerson@southeasttech.edu>
2024-04-17 20:27:44 -0500

    Add project files.

---
+++ Bookstore/Migrations/20240412161040_firstNameLastNameUserProps.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ch16Ex1Bookstore.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstNameLastNameUserProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "firstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "lastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "firstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "lastName",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Bookstore/Models/DomainModels/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Bookstore.Models
+{
+    public class User : IdentityUser
+    {
+        [NotMapped]
+        public IList<string> RoleNames { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
